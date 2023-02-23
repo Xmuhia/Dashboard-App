a9472382c68d342e723531048e5859cd866c4eb2
muhia <109690494+Xmuhia@users.noreply.github.com>
2023-02-23 14:13:39 +0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,4 +6,43 @@ const Sidebar = () => {
   )
 }
 
+export default Sidebarimport React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { BiCartAlt } from "react-icons/bi";
+import { MdOutlineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { links } from '../data/dummy';
+  
+const Sidebar = () => {
+  const activeMenu = true;
+
+  return (
+    <div className='ml-3 h-screen 
+    md:overflow-hidden overflow-auto 
+    md:hover:overflow-auto pb-10' > 
+      {activeMenu && (<>
+          <div className='flex justify-between items-center' >
+            <Link to='/' onClick={() => {}} 
+            className='items-center gap-3 ml-3 
+            mt-4 flex text-xl font-extrabold tracking-tight
+            dark:text-white text-slate-900'>
+              <BiCartAlt/> <span> ShopBud </span>
+            </Link>
+            <TooltipComponent content='Menu' position='BottomCenter' >
+              <button type='button' 
+              onClick={() => {}}
+              className='text-x1 rounded-full 
+              p-3 hover:bg-light-gray 
+              block md:hidden'
+              >
+                <MdOutlineCancel/>
+              </button>
+            </TooltipComponent>
+          </div>
+      </>)}
+    </div>
+  );
+}
+
 export default Sidebar
